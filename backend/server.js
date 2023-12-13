35b90f5050ebd96f0aa4c8e65c24a91d662ad6f4
tkt08 <tkt151221@gmail.com>
2023-12-13 15:15:27 +0900

    csvファイルに6軸のデータを保存

--- backend/server.js
+++ backend/server.js
@@ -4,35 +4,29 @@ const fs = require('fs');
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 
-// CSVファイルのヘッダー
-const headers = 'Timestamp,SensorValue\n';
 const csvFilePath = 'SensorData.csv';
+const headers = 'Timestamp,AccX,AccY,AccZ,GyroX,GyroY,GyroZ\n';
 
-// シリアルポート設定 115200bps
 const port = new SerialPort({ path: "COM9", baudRate: 115200 });
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
-// CSVファイルが存在しない場合は、新しくファイルを作成してヘッダーを書き込む
 if (!fs.existsSync(csvFilePath)) {
-  fs.writeFileSync(csvFilePath, headers, (err) => {
-    if (err) throw err;
-  });
+  fs.writeFileSync(csvFilePath, headers);
 }
 
 port.on('open', () => {
   console.log('ポートを開けました。');
 });
 
-// 受信データのハンドリング
 parser.on('data', (data) => {
-  console.log('受信データ:', data);
-  const timestamp = new Date().toISOString(); // ISO形式のタイムスタンプ
-  const sensorValue = parseFloat(data).toFixed(2); // センサー値を小数点2桁で丸める
-  const csvData = `${timestamp},${sensorValue}\n`; // CSV形式の文字列
+  const timestamp = new Date().toISOString();
+  const values = data.split(','); // 受信データをカンマで分割
 
-  // CSVファイルに追記
-  fs.appendFile(csvFilePath, csvData, (err) => {
-    if (err) throw err;
+  if (values.length === 6) { // 6つの値があることを確認
+    const csvData = `${timestamp},${values.join(',')}\n`;
+    fs.appendFileSync(csvFilePath, csvData);
     console.log('CSVにデータを追記しました。');
-  });
+  } else {
+    console.error('無効なデータを受信しました:', data);
+  }
 });

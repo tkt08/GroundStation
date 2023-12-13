725c5ee67c60651403b2120218a9c3d802a79bf9
tkt08 <tkt151221@gmail.com>
2023-12-13 22:28:26 +0900

    6軸の値を描画

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,12 +17,40 @@ export default defineComponent({
   data() {
     return {
       chartData: {
-        labels: [], // データポイントのラベル（例：タイムスタンプ）
+        labels: [],
         datasets: [
           {
-            label: 'センサーデータ',
-            data: [], // グラフに描画するデータ
+            label: 'acc_x',
+            data: [],
+            borderColor: 'red',
             // その他のグラフ設定
+          },
+          {
+            label: 'acc_y',
+            data: [],
+            borderColor: 'green',
+            // その他のグラフ設定
+          },
+          {
+            label: 'acc_z',
+            data: [],
+            borderColor: 'blue',
+            // その他のグラフ設定
+          },
+          {
+            label: 'gyro_x',
+            data: [],
+            borderColor: 'orange',
+          },
+          {
+            label: 'gyro_y',
+            data: [],
+            borderColor: 'purple',
+          },
+          {
+            label: 'gyro_z',
+            data: [],
+            borderColor: 'yellow',
           }
         ]
       },
@@ -35,16 +63,22 @@ export default defineComponent({
   mounted() {
     this.ws = new WebSocket('ws://localhost:8080');
 
-    this.ws.onmessage = (event) => {
-      console.log('受信したデータ：', event.data);
-      // センサーデータを受信し、グラフを更新
-      const value = parseFloat(event.data);
-      if (!isNaN(value)) {
-        this.chartData.labels.push(new Date().toLocaleTimeString());
-        this.chartData.datasets[0].data.push(value);
-        this.chartData = { ...this.chartData }; // リアクティブなデータ更新
-      }
-    };
+  this.ws.onmessage = (event) => {
+  const values = event.data.split(',').map(Number);
+  if (values.length >= 6) {
+    const [accX, accY, accZ, gyroX, gyroY, gyroZ] = values;
+    const label = new Date().toLocaleTimeString();
+    this.chartData.labels.push(label);
+    this.chartData.datasets[0].data.push(accX);
+    this.chartData.datasets[1].data.push(accY);
+    this.chartData.datasets[2].data.push(accZ);
+    this.chartData.datasets[3].data.push(gyroX);
+    this.chartData.datasets[4].data.push(gyroY);
+    this.chartData.datasets[5].data.push(gyroZ);
+    this.chartData = { ...this.chartData }; // リアクティブなデータ更新
+  }
+};
+
   }
 });
 </script>

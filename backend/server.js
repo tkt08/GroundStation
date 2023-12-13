b07ce4bc0ecc53748ae1aa52689ab3976abd2a5a
tkt08 <tkt151221@gmail.com>
2023-12-13 15:31:24 +0900

    UTCから日本時間に変換

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,29 @@ port.on('open', () => {
   console.log('ポートを開けました。');
 });
 
-parser.on('data', (data) => {
-  const timestamp = new Date().toISOString();
-  const values = data.split(','); // 受信データをカンマで分割
+// UTC時間を日本時間に変換し、秒数までのフォーマットで出力する関数
+function toJapanTime(date) {
+  const japanTimeOffset = 9 * 60; // 日本はUTC+9時間
+  // ローカルタイムゾーンのオフセットを取得し、UTC時間に変換
+  const utcTime = new Date(date.getTime() + (date.getTimezoneOffset() * 60000));
+  // UTC時間に日本のタイムゾーンオフセットを加算
+  const localTime = new Date(utcTime.getTime() + japanTimeOffset * 60000);
 
-  if (values.length === 6) { // 6つの値があることを確認
+  const year = localTime.getFullYear();
+  const month = ('0' + (localTime.getMonth() + 1)).slice(-2);
+  const day = ('0' + localTime.getDate()).slice(-2);
+  const hours = ('0' + localTime.getHours()).slice(-2);
+  const minutes = ('0' + localTime.getMinutes()).slice(-2);
+  const seconds = ('0' + localTime.getSeconds()).slice(-2);
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+parser.on('data', (data) => {
+  const timestamp = toJapanTime(new Date());
+  const values = data.split(',');
+
+  if (values.length === 6) {
     const csvData = `${timestamp},${values.join(',')}\n`;
     fs.appendFileSync(csvFilePath, csvData);
     console.log('CSVにデータを追記しました。');
